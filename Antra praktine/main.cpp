b2cc4ec3dd77c7b0bef755365b6edd7289d1d8d5
Danielius Jarmolovičius <s056908@ad.viko.lt>
2024-10-11 09:40:10 +0300

    2 upd

--- Antra praktine/main.cpp
+++ Antra praktine/main.cpp
@@ -2,46 +2,64 @@
 #include <random>
 using namespace std;
 
- void (Balses_pribalses){
- }
-
-int main() {
-    //string pavadinimas;
-    //int menu_options = 1;
-    /// while(menu_options !=5) {
-    //  cout << " 1. Balse ar priebalse? " << endl;
-    //cout << " 2. Maziausias bendras daliklis " << endl;
-    //cout << " 3. Zaidimas 1 - 100 " << endl;
-    //cout << " 4. FizzBuzz " << endl;
-    // cout << " 5. Iseiti"<< endl;
-    // cout << " Pasirinkite jus dominancia opcija" << endl;
-    // cin >> menu_options;
-    // }
-    int menu_options;
-    do {
-        cout << " Pasirinkite jus dominancia opcija" << endl;
-        cin >> menu_options;
-
-        switch (menu_options) {
-            case 1:
-                Balses_pribalses();
-            break;
-            case 2:
-                Maziausias_bendras_daliklis();
-            break;
-            case 3:
-                Zaidimas_1_100();
-            break;
-            case 4:
-                FizzBuzz();
-            break;
-            case 5:
-                cout << " Iseiti " << endl;
-            break;
-            default:
-                cout << " Neteisingas pasirinkimas. Bandykite dar karta " << endl;
+bool Balse(char letter){
+       char balse[]={'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+       for(int i=0;i<10;i++) {
+        if(letter==balse[i]) {
+         return true;
         }
-    }while (menu_options !=5);
+       }
+       return false;
+      }
+
+int BendrDal(int skaicius1, int skaicius2) {
+ while (skaicius2 !=0) {
+  int liekana = skaicius1 % skaicius2;
+  skaicius1 = skaicius2;
+  skaicius2 = liekana;
+ }
+ return skaicius1;
+}
+int main() {
+    string pavadinimas;
+    char letter;
+    int menu_options = 1;
+    while(menu_options !=5) {
+
+     cout << " 1. Balse ar priebalse? " << endl;
+     cout << " 2. Maziausias bendras daliklis " << endl;
+     cout << " 3. Zaidimas 1 - 100 " << endl;
+     cout << " 4. FizzBuzz " << endl;
+     cout << " 5. Iseiti"<< endl;
+     cout << " Pasirinkite jus dominancia opcija" << endl;
+     cin >> menu_options;
+
+     switch (menu_options) {
+
+      case 1:
+       cout << " Irasykite raide " << endl;
+       cin >> letter;
+      if(Balse(letter)) {
+       cout << letter << " Yra balse. " << endl;
+      }else {
+       cout << letter << " Ya priebalse" << endl;
+      }
+      break;
+      case 2:
+       int skaicius1, skaicius2;
+
+      cout << " Iveskite pirma skaiciu ";
+      cin >> skaicius1;
+      cout << " Iveskite antra skaiciu ";
+      cin >> skaicius2;
+      int DBD = BendrDal(skaicius1, skaicius2);
+      cout << " Didziausias bendras daliklis : " << DBD << endl;
+
+
+     }
+    }
+
+
         return 0;
 
 }
