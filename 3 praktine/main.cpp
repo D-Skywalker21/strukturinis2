b3e94d2edaf886d8ba2a405effa3e6ea85b17ce6
Danielius Jarmolovičius <s056908@ad.viko.lt>
2024-11-27 21:29:53 +0200

    kodas bendrai pakeistas, naudojamas masyvas, pakeista skaiciavimo formule

--- 3 praktine/main.cpp
+++ 3 praktine/main.cpp
@@ -1,139 +1,159 @@
 #include <iostream>
-#include <string>
-#include <cctype>
-
+#include <cstring>
 using namespace std;
 
-string vigenereEncrypt(const string& text, const string& key) {
-    string encryptedText = "";
-    int n = 26;
+const char Alphabet[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
-    int KeyIndex = 0;
-    for (int i = 0; i < text.length(); i++) {
-        char currentLetter = toupper(text[i]);
-        if (currentLetter < 'A' || currentLetter > 'Z') {
-            encryptedText += currentLetter;
-            continue;
+const int ASCII_MIN = 33, ASCII_MAX = 126;
+const int ASCII_RANGE = ASCII_MAX - ASCII_MIN + 1;
+
+int findAlphabetIndex(char letter) {
+    letter = toupper(letter);
+    for (int i = 0; i < 26; i++) {
+        if (Alphabet[i] == letter) {
+            return i;
         }
-        int m = currentLetter - 'A';
-        char KeyChar = toupper(key[KeyIndex % key.length()]);
-        int k = KeyChar - 'A';
-        int c = (m + k) % n;
-        encryptedText += 'A' + c;
-        KeyIndex++;
     }
-    return encryptedText;
+    return -1;
 }
 
-string vigenereDecrypt(const string& encryptedText, const string& key) {
-    string decryptedText = "";
-    int n = 26;
+int findAsciiIndex(char character) {
+    return static_cast<int>(character) - ASCII_MIN;
+}
 
-    int keyIndex = 0;
-    for (int i = 0; i < encryptedText.length(); i++) {
-        char currentLetter = toupper(encryptedText[i]);
-        if (currentLetter < 'A' || currentLetter > 'Z') {
-            continue;
+char getAlphabetChar(int index) {
+    return Alphabet[index % 26];
+}
+
+char getAsciiChar(int index) {
+    return static_cast<char>(index + ASCII_MIN);
+}
+
+void VigenereEncrypt(bool isAlphabet, const char key[], const char input[]) {
+    int key_len = strlen(key);
+    int input_len = strlen(input);
+    char extended_key[input_len];
+
+    for (int i = 0; i < input_len; i++) {
+        extended_key[i] = key[i % key_len];
+    }
+
+    char result[input_len];
+
+    for (int i = 0; i < input_len; i++) {
+        char currentChar = input[i];
+        char keyChar = extended_key[i];
+        int newIndex;
+
+        if (isAlphabet) {
+            int currentIndex = findAlphabetIndex(currentChar);
+            int keyIndex = findAlphabetIndex(keyChar);
+
+            if (currentIndex == -1 || keyIndex == -1) {
+                result[i] = currentChar;
+                continue;
+            }
+
+            newIndex = (currentIndex + keyIndex) % 26;
+            result[i] = getAlphabetChar(newIndex);
+        } else {
+            int currentIndex = findAsciiIndex(currentChar);
+            int keyIndex = findAsciiIndex(keyChar);
+
+            newIndex = (currentIndex + keyIndex) % ASCII_RANGE;
+            result[i] = getAsciiChar(newIndex);
         }
-
-        int c = currentLetter - 'A';
-        char raktasChar = toupper(key[keyIndex % key.length()]);
-        int k = raktasChar - 'A';
-        int m = (c - k + n) % n;
-
-        decryptedText += 'A' + m;
-        keyIndex++;
     }
-    return decryptedText;
+
+    cout << "Uzsifruotas zodis: ";
+    for (int i = 0; i < input_len; i++) {
+        cout << result[i];
+    }
+    cout << endl;
 }
 
-string vigenereEncryptAscii(const string& text, const string& key) {
-    string encryptedText = "";
-    int keyIndex = 0;
+void VigenereDecrypt(bool isAlphabet, const char key[], const char input[]) {
+    int key_len = strlen(key);
+    int input_len = strlen(input);
+    char extended_key[input_len];
 
-    for (int i = 0; i < text.length(); i++) {
-        char currentLetter = text[i];
-
-        int m = currentLetter;
-        int k = key[keyIndex % key.length()];
-        int c = (m + k) % 256;
-
-        encryptedText += char(c);
-        keyIndex++;
+    for (int i = 0; i < input_len; i++) {
+        extended_key[i] = key[i % key_len];
     }
 
-    return encryptedText;
-}
+    char result[input_len];
 
-string vigenereDecryptAscii(const string& encryptedText, const string& key) {
-    string decryptedText = "";
-    int keyIndex = 0;
+    for (int i = 0; i < input_len; i++) {
+        char currentChar = input[i];
+        char keyChar = extended_key[i];
+        int newIndex;
 
-    for (int i = 0; i < encryptedText.length(); i++) {
-        char currentLetter = encryptedText[i];
+        if (isAlphabet) {
+            int currentIndex = findAlphabetIndex(currentChar);
+            int keyIndex = findAlphabetIndex(keyChar);
 
-        int c = currentLetter;
-        int k = key[keyIndex % key.length()];
-        int m = (c - k + 256) % 256;
+            if (currentIndex == -1 || keyIndex == -1) {
+                result[i] = currentChar;
+                continue;
+            }
 
-        decryptedText += char(m);
-        keyIndex++;
+            newIndex = (currentIndex - keyIndex + 26) % 26;
+            result[i] = getAlphabetChar(newIndex);
+        } else {
+            int currentIndex = findAsciiIndex(currentChar);
+            int keyIndex = findAsciiIndex(keyChar);
+
+            newIndex = (currentIndex - keyIndex + ASCII_RANGE) % ASCII_RANGE;
+            result[i] = getAsciiChar(newIndex);
+        }
     }
 
-    return decryptedText;
+    cout << "Desifruotas zodis: ";
+    for (int i = 0; i < input_len; i++) {
+        cout << result[i];
+    }
+    cout << endl;
 }
 
 int main() {
+    int option;
+    while (true) {
+        cout << "1. Sifravimas" << endl;
+        cout << "2. Desifravimas" << endl;
+        cout << "3. Isejimas" << endl;
+        cout << "Pasirinkite: ";
+        cin >> option;
 
-    int choice = 1;
+        if (option == 3) break;
 
-    string text, key;
-    while (choice != 3) {
-        cout << "Vigenere sifravimo programa" << endl;
-        cout << "Pasirinkite opcija:" << endl;
-        cout << "1. Sifruoti/Desifruoti naudojant anglu abecele" << endl;
-        cout << "2. Sifruoti/Desifruoti naudojant ASCII vertes" << endl;
-        cout << "Pasirinkite (1 ar 2): ";
+        bool isAlphabet;
+        cout << "Pasirinkite sifravimo/desifravimo buda: " << endl;
+        cout << "1. Abecele" << endl;
+        cout << "2. ASCII" << endl;
+        int methodOption;
+        cin >> methodOption;
 
-        cin >> choice;
-        cin.ignore();
-        if (choice == 1) {
-            cout << "Iveskite ka norite uzsifruoti/desifruoti: ";
-            getline(cin, text);
-            cout << "Iveskite rakta: ";
-            getline(cin, key);
-
-            int action;
-            cout << "Pasirinkite veiksma (1 - uzsifruoti, 2 - desifruoti): ";
-            cin >> action;
-
-            if (action == 1) {
-                string encryptedText = vigenereEncrypt(text, key);
-                cout << "Uzsifruotas tekstas: " << encryptedText << endl;
-            } else if (action == 2) {
-                string decryptedText = vigenereDecrypt(text, key);
-                cout << "Desifruotas tekstas: " << decryptedText << endl;
-            }
-        } else if (choice == 2) {
-            cout << "Iveskite ka norite uzsifruoti/desifruoti: ";
-            getline(cin, text);
-            cout << "Iveskite rakta: ";
-            getline(cin, key);
-
-            int action;
-            cout << "Pasirinkite veiksma (1 - uzsifruoti, 2 - desifruoti): ";
-            cin >> action;
-
-            if (action == 1) {
-                string encryptedText = vigenereEncryptAscii(text, key);
-                cout << "Uzsifruotas tekstas: " << encryptedText << endl;
-            } else if (action == 2) {
-                string decryptedText = vigenereDecryptAscii(text, key);
-                cout << "Desifruotas tekstas: " << decryptedText << endl;
-            }
+        if (methodOption == 1) {
+            isAlphabet = true;
+        } else if (methodOption == 2) {
+            isAlphabet = false;
         } else {
-            cout << "Klaida!" << endl;
+            cout << "Klaida! Pasirinkite teisingą metodą." << endl;
+            continue;
+        }
+
+        char key[50], input[50];
+        cout << "Iveskite rakta: ";
+        cin >> key;
+        cout << "Iveskite pradini zodi: ";
+        cin >> input;
+
+        if (option == 1) {
+            VigenereEncrypt(isAlphabet, key, input);
+        } else if (option == 2) {
+            VigenereDecrypt(isAlphabet, key, input);
         }
     }
+
     return 0;
 }
